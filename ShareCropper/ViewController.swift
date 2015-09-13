989c2d8d4fdcfe212bfca42a74ae1731eb14942c
abwayland <abwayland@gmail.com>
2015-09-12 21:02:06 -0400

    Initial Commit

---
+++ ShareCropper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareCropper
+//
+//  Created by Adam Wayland on 9/12/15.
+//  Copyright (c) 2015 Adam Wayland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
