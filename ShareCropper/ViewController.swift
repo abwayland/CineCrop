1fd965b2286a2811c5fb4267c75ad774872c13aa
abwayland <abwayland@gmail.com>
2015-09-14 21:23:07 -0400

    Initial Commit

--- ShareCropper/ViewController.swift
+++ ShareCropper/ViewController.swift
@@ -5,13 +5,84 @@
 //  Created by Adam Wayland on 9/12/15.
 //  Copyright (c) 2015 Adam Wayland. All rights reserved.
 //
+//  ShareCropper is an image and potentially video cropping app that provides the standard film aspect ratios as well as custom aspect ratios.
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let academy = 1.33
+    let europeanWS = 1.66
+    let sixteenNine = 1.77
+    let americanWS = 1.85
+    let seventyMM = 2.2
+    let anamorphic = 2.39 //could be 2.35
+    let cinerama = 2.77
+    var ratioArr: [String] = []
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var imageView: UIImageView!
+    //var newMedia: Bool? //Use when implement camera
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return ratioArr.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        return NSAttributedString(string: ratioArr[row], attributes: [NSForegroundColorAttributeName:UIColor.whiteColor()])
+    }
+    
+    @IBAction func useCameraRoll(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum) {
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            imagePicker.mediaTypes = [kUTTypeImage as NSString]
+            imagePicker.allowsEditing = false
+            
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+            
+            //newMedia = false
+        }
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! String
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        if mediaType == kUTTypeImage as! String {
+            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+            imageView.image = image
+            
+//            if (newMedia == true) {
+//                UIImageWriteToSavedPhotosAlbum(image, self,
+//                    "image:didFinishSavingWithError:contextInfo:", nil)
+//            } else if mediaType.isEqualToString(kUTTypeMovie as! String) {
+//                // Code to support video here
+//            }
+        }
+        
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ratioArr = ["Academy (1.33:1)","European (1.66:1)","HD (16:9)","American WS (1.85:1)","70mm (2.2:1)","Anamorphic WS (2.39:1)","Cinerama (2.77:1)"]
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        view.backgroundColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        imageView.backgroundColor = UIColor(red: 0.3, green: 0.3, blue: 0.3, alpha: 1)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
